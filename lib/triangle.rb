6d197a97292d8c569f69263ce5e412ecc70df67d
Mikel Bishop <mdrew8@gmail.com>
2020-09-24 23:46:11 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,32 @@
 class Triangle
   
-  attr_accessor :side_1, :side_2, :side_3
+  attr_accessor :side_1, :side_2, :side_3, :sides 
+  
+  @sides = []
   
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
+    @sides = [side_1, side_2, side_3] 
+    @sides.sort!
   end 
   
+  def kind 
+    if side_1 <= 0 || side_2 <= 0 || side_3 <= 0 
+      raise TriangleError
+    elsif ((@sides[0] + @sides[1]) <= @sides[2])
+      raise TriangleError
+    elsif side_1 == side_2 && side_2 == side_3
+      :equilateral 
+    elsif side_1 == side_2 || side_2 == side_3 || side_3 == side_1
+      :isosceles
+    else 
+      :scalene 
+    end 
+  end 
   
-  
+  class TriangleError < StandardError
+  end 
   
 end
